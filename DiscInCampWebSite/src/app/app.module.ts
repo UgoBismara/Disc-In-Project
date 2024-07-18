987e55dc7ab73887c765d9e500341441d7dd7534
ubismara <ugo.bismara@soprasteria.com>
2024-07-18 14:49:13 +0200

    Ajout authentification

---
+++ DiscInCampWebSite/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app.routes';
+import { AppComponent } from './app.component';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  HTTP_INTERCEPTORS,
+  HttpClient,
+  HttpClientModule,
+} from '@angular/common/http';
+import { AuthentificationComponent } from './authentification/authentification.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AuthentificationComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+  ],
+  providers: [
+
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
--- DiscInCampWebSite/src/app/app.routes.ts
+++ DiscInCampWebSite/src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthentificationComponent } from './authentification/authentification.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'Authentification',
+    component: AuthentificationComponent,
+  }
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
